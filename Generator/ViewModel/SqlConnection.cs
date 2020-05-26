41504cf4182667216705b0061ec15a430bb9d511
mateuszszolc <51001128+mateuszszolc@users.noreply.github.com>
2020-05-26 22:05:55 +0200

    Update SqlConnections.cs

--- Generator/ViewModel/SqlConnection.cs
+++ Generator/ViewModel/SqlConnection.cs
@@ -8,9 +8,9 @@ namespace Generator.ViewModel
 {
     public static class SqlConnection
     {
-        public static string connectionString { get; } = "Data Source = 217.173.198.135:1522/orcltp.iaii.local;" +
-            " User ID = s95567;" +
-            " Password = s95567";
+        public static string connectionString { get; } = "Data Source = exampleServerIP;" +
+            " User ID = ExampleUser;" +
+            " Password = ExampleUser";
 
     }
 }
